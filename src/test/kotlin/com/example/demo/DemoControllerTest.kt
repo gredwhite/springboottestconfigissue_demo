3525bab66ff2bcffe595e982f8786c943a7c697e
NTkachev <ntkachev@inno.tech>
2023-09-29 19:30:39 +0300

    fix

--- src/test/kotlin/com/example/demo/DemoControllerTest.kt
+++ src/test/kotlin/com/example/demo/DemoControllerTest.kt
@@ -2,11 +2,14 @@ package com.example.demo
 
 import com.example.demo.app.MyConfigurationTest
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.context.annotation.Import
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
